324022bd9c57de1610658b65fbd514b772c84ed6
bmangust <ftf.pollux@gmail.com>
2020-04-13 09:28:23 +0300

    node js visualiser - menu done; data flow WIP

---
+++ visualiser/app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const readline = require("readline");
+const app = express();
+//const { spawn } = require('child_process');
+const resourcepath = '../resources';
+const bodyParser = require("body-parser");
+//const fillervm = spawn(`${resourcepath}/filler_vm -f ${resourcepath}/maps/map00 -p1 ${resourcepath}/players/akraig.filler -p2 ${resourcepath}/players/carli.filler`);
+
+const path = `${__dirname}/server`;
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+app.use(express.static(path));
+
+app.get("/", function(request, response){
+    response.sendFile(__dirname + "/server/index.html");
+});
+app.get("/game", function(request, response){
+    response.sendFile(__dirname + "/server/game.html");
+});
+app.get("/settings", function(request, response){
+    response.sendFile(__dirname + "/server/settings.html");
+});
+app.get("/about", function(request, response){
+    response.sendFile(__dirname + "/server/about.html");
+});
+app.post("/settings", urlencodedParser, function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    response.send(`players: ${request.body.player1} and ${request.body.player2}, map: ${request.body.map}`);
+});
+
+rl.on('line', (input) => {
+	if (readingMap) {
+		readMap(input);
+	}
+	switch (input) {
+		case 'get map':
+			printMap();
+			break;
+		case 'get maparr':
+			log(getMap());
+			break;
+		case 'get params':
+			rl.write(`width: ${width}, height: ${height}\n`);
+			break;
+		case 'get player':
+			rl.write(`we're number ${playerNumber}\n`);
+			break;
+		default:
+			processInput(input);
+	}
+});
+
+app.listen(3000);
